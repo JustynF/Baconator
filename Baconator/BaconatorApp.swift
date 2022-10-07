1156b66f25cab86870bc03e5645dde8f9da7fdcc
justynf <justynflorendo@gmail.com>
2022-10-06 21:07:45 -0400

    initial commit

---
+++ Baconator/BaconatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaconatorApp.swift
+//  Baconator
+//
+//  Created by justyn on 2022-10-06.
+//
+
+import SwiftUI
+
+@main
+struct BaconatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Baconator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Baconator
+//
+//  Created by justyn on 2022-10-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
